5f63664de05b2855e9803f2b90438c272cdfeac2
bhartik021 <bhartik021@gmail.com>
2022-07-19 10:38:34 +0530

    icons

--- src/App.js
+++ src/App.js
@@ -41,6 +41,35 @@ calCelsius(temp) {
   return cell;
 }
 
+get_WeatherIcon(icons, rangeId) {
+  switch (true) {
+    case rangeId >= 200 && rangeId < 232:
+      this.setState({ icon: icons.Thunderstorm });
+      break;
+    case rangeId >= 300 && rangeId <= 321:
+      this.setState({ icon: icons.Drizzle });
+      break;
+    case rangeId >= 500 && rangeId <= 521:
+      this.setState({ icon: icons.Rain });
+      break;
+    case rangeId >= 600 && rangeId <= 622:
+      this.setState({ icon: icons.Snow });
+      break;
+    case rangeId >= 701 && rangeId <= 781:
+      this.setState({ icon: icons.Atmosphere });
+      break;
+    case rangeId === 800:
+      this.setState({ icon: icons.Clear });
+      break;
+    case rangeId >= 801 && rangeId <= 804:
+      this.setState({ icon: icons.Clouds });
+      break;
+    default:
+      this.setState({ icon: icons.Clouds });
+  }
+}
+
+
 getWeather = async () => {
   const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
 
@@ -55,8 +84,8 @@ getWeather = async () => {
     temp_max: this.calCelsius(response.main.temp_max),
     temp_min: this.calCelsius(response.main.temp_min),
     description : response.weather[0].description,
-    icon : this.weatherIcon.Thunderstorm
-  })
+  });
+  this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
 };
 
   render() {
